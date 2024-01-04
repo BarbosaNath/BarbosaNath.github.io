8efd27b83c324f83869423df30491d0f87bb13cf
Nath <playerxmatt@gmail.com>
2024-01-04 12:30:27 -0300

    feat: created basic SPA structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import LandingPage from "./components/LandingPage";
 
 function App() {
   return (
     <>
+      <Header />
+
+      <LandingPage />
+
+      <Footer />
     </>
   );
 }
